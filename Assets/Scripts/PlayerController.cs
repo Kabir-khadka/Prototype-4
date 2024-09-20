7a5310c0f7bef66c8700441e9d40051ff9479e00
Kabir-khadka <kabirkhadka50@gmail.com>
2024-09-20 09:23:11 +0545

    Added powerup UI
    New Concepts and Skills:
    Debug concatenation
    Local component variables
    IEnumerators and WaitForSeconds()
    Coroutines
    SetActive(true/false)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
+    public GameObject powerupIndicator;
 
+    private float sentAwayForce = 15.0f;
     private GameObject focalPoint; 
     private Rigidbody playerRb;
 
-    private bool hasPowerUp = false;//creating a bool variable to check the collision condition.
+    public bool hasPowerUp = false;//creating a bool variable to check the collision condition.
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,10 +34,12 @@ public class PlayerController : MonoBehaviour
 
         //Moving the player in forward direction by adding force to the rigid body
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.58f, 0);
         
     }
 
-    private void OnCollisionEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
 
         //creating logic uisng if statement to check the collision with the powerup
@@ -42,7 +47,33 @@ public class PlayerController : MonoBehaviour
         {
 
             hasPowerUp = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+
+    //using this method to check for the condition to dectect the collisoin with enemy.
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * sentAwayForce, ForceMode.Impulse);
+            Debug.Log("Collided with " + collision.gameObject.name + (" with powerup set to " + hasPowerUp));
         }
 
     }
